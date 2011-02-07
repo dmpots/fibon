20670dd63925b9001d9ca5cdd5e3b9c45f86470d
David M Peixotto <dmp@rice.edu>
2011-02-07 16:51:03 -0600

    Sort configIds and benchmark names in manifest output

--- tools/fibon-run/Fibon/Run/Manifest.hs
+++ tools/fibon-run/Fibon/Run/Manifest.hs
@@ -5,6 +5,7 @@ module Fibon.Run.Manifest (
 )
 where
 
+import Data.Function
 import Data.List
 import Fibon.Benchmarks
 import Fibon.Run.Config
@@ -12,10 +13,11 @@ import Fibon.Run.Config.Default as Default
 import Fibon.Run.Config.Local as Local
 
 configManifest :: [RunConfig]
-configManifest = Default.config : Local.configs
+configManifest = order $ Default.config : Local.configs
+  where order = sortBy (compare `on` configId)
 
 groupManifest :: [FibonGroup]
 groupManifest = (nub . sort . map benchGroup)  benchmarkManifest
 
 benchmarkManifest :: [FibonBenchmark]
-benchmarkManifest = allBenchmarks
+benchmarkManifest = sort allBenchmarks
